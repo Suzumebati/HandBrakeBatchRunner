24a21b1faae72c5237f641dbbbd4c0a7ce34c38e
suzumebati <51582636+Suzumebati@users.noreply.github.com>
2019-12-24 14:42:56 +0900

    コンスタント定義追加

---
+++ src/HandBrakeBatchRunner/HandBrakeBatchRunner/Constant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandBrakeBatchRunner
+{
+    public static class Constant
+    {
+        public static string CONVERT_SETTING_FILE_NAME = "ConvertSettings.xml";
+    }
+}
